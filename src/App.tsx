24d77f627026cf491ea823e0aee0c3d651434f54
FrederikVA <s224308@student.dtu.dk>
2024-02-12 19:51:09 +0100

    added productlist from github

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,15 +7,20 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <h1>Fruit boys</h1>
+      </header>
+      <div className='priceInfo'>
+        <div> 
+          total price
+        </div>
+        <div> 
+          tax
+        </div>
+        <div> 
+          discount
+        </div>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
